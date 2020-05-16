75862fc10a5ad84ba166680a1b0389aa04394e92
agparkes <agparkes413@gmail.com>
2020-05-16 16:14:41 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Scrape Books Website
+
+This project is about automating the scraping process of a book site by taking advantage of an powerful tool called Scrapy. It is a framework for extracting data, and writing the data to a file and a number of other useful features made easy.
+
+The project will use Spiders, which according to https://docs.scrapy.org/en/latest/topics/spiders.html, "... are classes which define how a certain site (or a group of sites) will be scraped, including how to perform the crawl (i.e. follow links) and how to extract structured data from their pages (i.e. scraping items)."
---
+++ scrape_website_books.py
@@ -0,0 +1,20 @@
+# http://books.toscrape.com/
+
+import scrapy
+
+class BooksSpider(scrapy.Spider):
+    name = "bookspider"
+    # allowed_domains = ['example.com']
+    start_urls = [
+        "http://books.toscrape.com"
+    ]
+
+    def parse(self, response):
+        for article in response.css("article.product_pod"):
+            yield {
+                "title": article.css("h3 > a::attr(title)").extract_first(),
+                " price": article.css(" .price_color::text").extract_first()
+            }
+            next = response.css(".next > a::attr(href)").extract_first()
+            if next:
+                yield response.follow(next, self.parse)
